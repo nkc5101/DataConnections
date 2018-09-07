5f3c29080e537f722a0034d20d670c9e1ec84390
wyattnaftulin <wyattnaftulin@10.0.0.31>
2018-09-07 01:11:35 -0400

    retrieve table

--- DataConnections/src/FBISiteParser.java
+++ DataConnections/src/FBISiteParser.java
@@ -7,4 +7,12 @@ import org.jsoup.select.Elements;
 
 public class FBISiteParser {
     
+    public void parse() throws IOException {
+        
+        Document document;
+        document = Jsoup.connect("https://ucr.fbi.gov/crime-in-the-u.s/2016/crime-in-the-u.s.-2016/topic-pages/tables/table-1").get();
+        Elements table = document.getElementsByTag("table");
+        
+    }
+    
 }
