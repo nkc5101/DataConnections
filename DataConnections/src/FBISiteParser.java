2ff26b55c900cc74d00abe23ab5f63c1cb1fd88f
wyattnaftulin <wyattnaftulin@10.0.0.31>
2018-09-07 01:00:16 -0400

    Downloaded jsoup, added to classpath, started importing some things.

---
+++ DataConnections/src/FBISiteParser.java
@@ -0,0 +1,10 @@
+
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class FBISiteParser {
+    
+}
---
+++ DataConnections/src/dataconnections/DataConnections.java
@@ -0,0 +1,12 @@
+package dataconnections;
+
+public class DataConnections {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
