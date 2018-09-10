271c5ce263dcd3da14a2208c6f24def8a81d67b1
nate <nate@Desktop.firelocked.com>
2018-09-10 12:59:08 -0400

    moved parse file

--- DataConnections/src/dataconnections/FBISiteParser.java
+++ DataConnections/src/dataconnections/FBISiteParser.java
@@ -1,8 +1,9 @@
+package dataconnections;
+
 
 import java.io.IOException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class FBISiteParser {
