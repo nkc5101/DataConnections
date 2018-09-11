638bf1d13c00c09ff199fd77233ed6676019a9c5
wyattnaftulin <wyattnaftulin@10.0.0.31>
2018-09-11 14:08:50 -0400

    prints headers and columns (now has import errors after pull, though)

--- DataConnections/src/dataconnections/FBISiteParser.java
+++ DataConnections/src/dataconnections/FBISiteParser.java
@@ -1,9 +1,10 @@
-package dataconnections;
 
+package dataconnections;
 
 import java.io.IOException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class FBISiteParser {
@@ -12,8 +13,15 @@ public class FBISiteParser {
         
         Document document;
         document = Jsoup.connect("https://ucr.fbi.gov/crime-in-the-u.s/2016/crime-in-the-u.s.-2016/topic-pages/tables/table-1").get();
-        Elements table = document.getElementsByTag("table");
         
+        Element table = document.select("table").get(0);
+        Elements rows = table.select("tr");
+        
+        for (int i = 1; i < rows.size(); i++) {
+            Element row = rows.get(i);
+            Elements columns = row.select("td");
+            System.out.println(columns);
+        }
     }
     
 }
